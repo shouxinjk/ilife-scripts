adbbecda8cda0eda22894f8ed4bd039a1a083380
Alex Chew <alex_chew@msn.com>
2018-09-11 14:47:11 +0800

    change file structure

--- dist/dist.js
+++ dist/dist.js
@@ -1,5 +1,42 @@
 //debug 
-var __debug=true;
+var debug=false;
 
 //data api
 var spi = 'https://data.shouxinjk.net/_db/sea/_api/document/';
+
+//create a new seed
+function sendUrl(data){
+	var url = spi + "seeds";
+	postData(url, data);
+}
+
+//create a new item
+function sendItem(data){
+	var url = spi + "items";
+	postData(url, data);
+}
+
+//this is a private method
+function postData(url,data){
+    var req = new XMLHttpRequest();
+    req.open('POST', url, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.setRequestHeader('Authorization', 'Basic aWxpZmU6aWxpZmU=');
+    req.setRequestHeader('Api-Key', 'foobar');
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status >= 200 && req.status < 400) {
+                if(debug)console.log(JSON.parse(req.responseText));
+            } else {
+                // Handle error case
+            }
+            //TODO here can put call back
+        }
+    };
+    data._key = hex_md5(data.url);
+    try{
+        req.send(JSON.stringify(data));//post data
+    }catch(e){
+        if(debug)console.log("Error while post data to create new document."+e);
+    }
+}
