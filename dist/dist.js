9dba3aa032a7b891b4a116d44bf8a931c01c8bb0
Alex Chew <alex_chew@msn.com>
2018-09-18 23:13:33 +0800

    improve: check if exists while posting data

--- dist/dist.js
+++ dist/dist.js
@@ -16,10 +16,31 @@ function commitData(data,callback){
 	postData(url, data,callback);
 }
 
+function postData(url,data,callback){
+    console.log("check if data exists.[spi]"+url,"[data.url]"+data.url);
+    var _key = hex_md5(data.url);
+    var req = new XMLHttpRequest();
+    req.open('GET', url+"/"+_key, true);//query to check if exists
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.setRequestHeader('Authorization', 'Basic aWxpZmU6aWxpZmU=');
+    req.setRequestHeader('Api-Key', 'foobar');
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status >= 200 && req.status < 400) {//yes the item exists
+                if(debug)console.log(JSON.parse(req.responseText));
+                update(url+"/"+_key,data,callback);
+            } else {//no the item does not exist
+                create(url,data,callback);
+            }
+        }
+    };
+    
+}
+
 //this is a private method
 //TODO: create a new one or update for an exist item
-function postData(url,data,callback){
-    console.log("commit to "+url,data);
+function create(url,data,callback){
+    console.log("create new document against "+url,data);
     var req = new XMLHttpRequest();
     req.open('POST', url, true);
     req.setRequestHeader('Content-Type', 'application/json');
@@ -41,6 +62,32 @@ function postData(url,data,callback){
     try{
         req.send(JSON.stringify(data));//post data
     }catch(e){
-        if(debug)console.log("Error while post data to create new document."+e);
+        if(debug)console.log("Error while update data to create new document."+e);
+    }
+}
+
+function update(url,data,callback){
+    console.log("update exist document against "+url,data);
+    var req = new XMLHttpRequest();
+    req.open('PATCH', url, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.setRequestHeader('Authorization', 'Basic aWxpZmU6aWxpZmU=');
+    req.setRequestHeader('Api-Key', 'foobar');
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status >= 200 && req.status < 400) {
+                if(debug)console.log(JSON.parse(req.responseText));
+            } else {
+                // Handle error case
+            }
+            if(callback && typeof callback == 'function'){
+                callback();
+            }
+        }
+    };
+    try{
+        req.send(JSON.stringify(data));//post data
+    }catch(e){
+        if(debug)console.log("Error while update data to create new document."+e);
     }
 }
