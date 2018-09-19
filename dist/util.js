9639ce69afecd5a007d5a5fd71cea0a8df89a1ed
Alex Chew <alex_chew@msn.com>
2018-09-19 10:15:21 +0800

    update to filter query

--- dist/util.js
+++ dist/util.js
@@ -1,46 +1,60 @@
 //debug 
-var debug=false;
+var debug=true;
 
 //data api
 var spi = 'https://data.shouxinjk.net/_db/sea/_api/document/';
+var spi_query = 'https://data.shouxinjk.net/_db/sea/_api/simple/by-example';
+var query=
+    {
+        collection: "items", 
+        example: { 
+            _key :null
+        } 
+    };
+
 //auth
 var auth = 'Basic aWxpZmU6aWxpZmU=';
 
 //create a new seed
 function commitUrl(data,callback){
-	var url = spi + "seeds";
-	__postData(url, data,callback);
+	__postData("seeds", data,callback);
 }
 
 //create a new item
 function commitData(data,callback){
-	var url = spi + "items";
-	__postData(url, data,callback);
+	__postData("items", data,callback);
 }
 
 //pivate method
 //check data and dispatch to create or update
-function __postData(url,data,callback){
+function __postData(collection,data,callback){
     if(debug)console.log("check if data exists.[spi]"+url,"[data.url]"+data.url);
     var _key = hex_md5(data.url);
     var req = new XMLHttpRequest();
-    req.open('GET', url+"/"+_key, true);//query to check if exists
+    query.collection = collection;
+    query.example._key = _key;
+    req.open('PUT', spi_query, true);//query to check if exists
     req.setRequestHeader('Content-Type', 'application/json');
     req.setRequestHeader('Authorization', auth);
     req.setRequestHeader('Api-Key', 'foobar');
     req.onreadystatechange = function() {
         if (req.readyState === 4) {
             if (req.status >= 200 && req.status < 400) {//yes the item exists
+                var result = JSON.parse(req.responseText);
+                if(debug)console.log(result);
+                if(result.length>0){
+                    __update(spi+"/"+collection+"/"+_key,data,callback);
+                }else{
+                    __create(spi+"/"+collection,data,callback);
+                }
+                
+            } else {//query error
                 if(debug)console.log(JSON.parse(req.responseText));
-                __update(url+"/"+_key,data,callback);
-            } else {//no the item does not exist
-                if(debug)console.log(JSON.parse(req.responseText));
-                __create(url,data,callback);
             }
         }
     };
     try{
-        req.send(JSON.stringify(data));//post data
+        req.send(JSON.stringify(query));//put query
     }catch(e){
         if(debug)console.log("Error while checking data if exists."+e);
     }
