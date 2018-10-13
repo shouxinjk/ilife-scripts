0fca1facd6ed52f72ca87d328c414fba29ae310c
Alex Chew <alex_chew@msn.com>
2018-10-13 21:54:32 +0800

    add getQuery method

--- dist/util.js
+++ dist/util.js
@@ -177,3 +177,24 @@ function __ajax(url,data,method="GET",callback){
         if(_debug)console.log("AJAX error."+e);
     }
 }
+
+
+//从链接中获取参数
+function _getQuery(full_url=window.location.search) {
+    //取得查询字符串并去掉开头的问号
+    var qs = full_url.length > 0 ? full_url.substring(1):"";
+    //保存数据的对象
+    var args = {};
+    //取得每一项
+    var items = qs.length > 0 ? qs.split('&'):[];
+    var item = null,name = null,value = null;
+    for(var i = 0;i < items.length;i++) {
+        item = items[i].split('=');
+        name = decodeURIComponent(item[0]);
+        value = decodeURIComponent(item[1]);
+        if(name.length) {
+            args[name] = value;
+        }
+    }
+    return args;
+}
