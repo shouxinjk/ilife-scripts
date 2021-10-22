e7b9b84893d1aa9be7a8c782fbb77fc69afa3226
Alex Chew <alex_chew@msn.com>
2021-10-22 14:50:42 +0800

    add cors cookie support

--- src/toolbar/mp.js
+++ src/toolbar/mp.js
@@ -14,28 +14,11 @@ function sxInitialize(jNode){
 
     //设置操作按钮：显示或隐藏
     var htmlBtn = "";
-    //读取面板显示状态
-    var sxShowListHelper = Cookies.get('sxShowListHelper');
-    console.log("\n\n==========load sxShowListHelper========\n\n",sxShowListHelper);
-    var isShowlistHelper = {show:true};//默认显示
-    var hideBtnDisplayStyle = "display:block";
-    var showBtnDisplayStyle = "display:none";
-    if(sxShowListHelper && sxShowListHelper.trim().length>0){
-        isShowlistHelper = JSON.parse(sxShowListHelper);
-        if(isShowlistHelper.show){
-            hideBtnDisplayStyle = "display:block";
-            showBtnDisplayStyle = "display:none";
-        }else{//如果设置了隐藏面板
-            hideBtnDisplayStyle = "display:none";
-            showBtnDisplayStyle = "display:block";
-        }
-    }
-
-    htmlBtn += '<div id="sxHideBtnDiv" class="info" style="position:fixed;z-index:999;top:140px;right:30px;background-color:#fff;width:50px;height:20px;border-radius:5px;'+hideBtnDisplayStyle+'">';
+    htmlBtn += '<div id="sxHideBtnDiv" class="info" style="position:fixed;z-index:999;top:140px;right:30px;background-color:#fff;width:50px;height:20px;border-radius:5px;display:none">';
     htmlBtn += '<div style="line-height:22px;color:grey;">隐藏</div>';
     htmlBtn += '</div>';
     //show btn
-    htmlBtn += '<div id="sxShowBtnDiv" class="info" style="position:fixed;z-index:999;top:130px;right:0;background-color:#fff;width:44px;height:150px;border-radius:5px;padding-left:12px;'+showBtnDisplayStyle+'">';
+    htmlBtn += '<div id="sxShowBtnDiv" class="info" style="position:fixed;z-index:999;top:130px;right:0;background-color:#fff;width:44px;height:150px;border-radius:5px;padding-left:12px;display:block">';
     htmlBtn += '<div class="info-general">';
     htmlBtn += '<img class="general-icon" src="'+brokerLogo+'" width="30" height="30"/>';
     htmlBtn += '</div>';
@@ -46,7 +29,7 @@ function sxInitialize(jNode){
     jNode.append(htmlBtn);
 
     var html = "";
-    html += "<div id='sxDiv' style='position:fixed;z-index:99;top:130px;right:30px;background-color:#fff;min-width:400px;width:20%;height:90%;border-radius:5px;"+hideBtnDisplayStyle+"'>";
+    html += "<div id='sxDiv' style='position:fixed;z-index:99;top:130px;right:30px;background-color:#fff;min-width:400px;width:20%;height:90%;border-radius:5px;display:none'>";
     //broker info
     html += '<div id="brokerInfoDiv" class="info">';
     html += '<div class="info-general">';
@@ -64,31 +47,51 @@ function sxInitialize(jNode){
     html += "</div>";
     jNode.append(html);
 
-    //知乎不能通过嵌入iframe运行：TODO：需要调整为原生方式显示
-    //loadSxHelperSource();
+    //读取面板显示状态：通过postMessage向iframe请求cookie得到
+    console.log("post message to get sxToolbarStatus ");
+    document.getElementById('sxListFrame').contentWindow.postMessage({
+      sxCookie:{
+        action:"get",
+        key:"sxToolbarStatus"
+      }
+    }, "*");//向父窗口发出消息，查询sxAuth
 
     //调整iframe高度
     var iframeHeight = $('#sxDiv').height()-$('#brokerInfoDiv').height();
     $("#sxListFrame").height(iframeHeight);
-
-    //监听iframe事件，在扫码回调后处理
-    $("#sxListFrame").load(function(event){
-        var src = $(this).attr("src");
-        console.log("iframe onload.[src]"+src,event);
-    });
 
     //隐藏事件
     $("#sxHideBtnDiv").click(function(event){
         $("#sxHideBtnDiv").css("display","none");
         $("#sxShowBtnDiv").css("display","block");
         $("#sxDiv").css("display","none");
-        Cookies.set('sxShowListHelper', JSON.stringify({show:false}));
+
+        //存入缓存，能够在多个界面中保持一致
+        var data = {
+            sxCookie:{
+                action: 'set',
+                key:"sxToolbarStatus",
+                value:{show:false}
+            }
+        };
+        console.log("try to post sxToolbar status message to iframe.",data);
+        document.getElementById('sxListFrame').contentWindow.postMessage(data, '*');
     });
     $("#sxShowBtnDiv").click(function(event){
         $("#sxShowBtnDiv").css("display","none");
         $("#sxHideBtnDiv").css("display","block");
         $("#sxDiv").css("display","block");
-        Cookies.set('sxShowListHelper', JSON.stringify({show:true}));
+
+        //存入缓存，能够在多个界面中保持一致
+        var data = {
+            sxCookie:{
+                action: 'set',
+                key:"sxToolbarStatus",
+                value:{show:true}
+            }
+        };
+        console.log("try to post sxToolbar status message to iframe.",data);
+        document.getElementById('sxListFrame').contentWindow.postMessage(data, '*');
     });
 }
 
@@ -101,7 +104,7 @@ function listenPostMessage(){
     eventer(messageEvent,function(e) {
         var key = e.message ? "message" : "data";
         var data = e[key];
-        console.log("got message",data);
+        console.log("got message from child window.",data);
         if(data&&data.sxBrokerName){//更新达人名称，同时显示 切换按钮。点击切换后将删除sxCookie
             var brokerName = "Hi,"+data.sxBrokerName+(data.sxBrokerRealName&&data.sxBrokerRealName.trim().length>0?("("+data.sxBrokerRealName+")"):"");
             brokerName += "&nbsp;<a href='#' style='font-size:12px;color:silver' id='sxChangeBroker' alt='切换账户'><img width='12' text='切换账户' style='vertical-align:middle; margin: 0 auto; ' src='https://www.biglistoflittlethings.com/ilife-web-wx/images/change.png'/></a>";
@@ -119,20 +122,22 @@ function listenPostMessage(){
             $("#sxListFrame").attr("src", data.sxNavigateTo );
         if(data&&data.sxCookie){//实现缓存数据交换
             var sxCookie  = data.sxCookie;//JSON.parse(data.sxCookie);
-            if (sxCookie.action == 'set'){
-                //window.localStorage.setItem(sxCookie.key, JSON.stringify(sxCookie.value))
-                Cookies.set(sxCookie.key, JSON.stringify(sxCookie.value));
-            } else if (sxCookie.action == 'get') {
-                console.log("try to post message to iframe.",data);
-                document.getElementById('sxListFrame').contentWindow.postMessage({
-                    sxCookie:{
-                        action: 'return',
-                        key:sxCookie.key,
-                        value:Cookies.get(sxCookie.key)?JSON.parse(Cookies.get(sxCookie.key)):{}
-                        //value:window.localStorage.getItem(sxCookie.key)?JSON.parse(window.localStorage.getItem(sxCookie.key)):{}
+            console.log("got message.",sxCookie);
+            if (sxCookie.action == 'return'){//从iframe cookie中查询获得数据
+                //如果是sxToolbarStatus 则修改显示状态
+                if (sxCookie.key == 'sxToolbarStatus'){
+                    var isShowlistHelper = sxCookie.value;
+                    if(isShowlistHelper.show){
+                        $("#sxHideBtnDiv").css("display","block");
+                        $("#sxDiv").css("display","block");
+                        $("#sxShowBtnDiv").css("display","none");
+                    }else{//如果隐藏面板
+                        $("#sxHideBtnDiv").css("display","none");
+                        $("#sxDiv").css("display","none");
+                        $("#sxShowBtnDiv").css("display","block");
                     }
-            }, '*');
-        }
+                }
+            }
         }
     },false);
 }
